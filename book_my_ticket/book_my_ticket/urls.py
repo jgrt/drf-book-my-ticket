57fee5da878926f51b10f3a9f2d271eed1b7ef23
jgrt <asati.jagrati93@gmail.com>
2020-09-16 18:25:32 +0530

    Added Django Project boom_my_ticket. Configured .env. Updated Setting file.

---
+++ book_my_ticket/book_my_ticket/urls.py
@@ -0,0 +1,44 @@
+"""book_my_ticket URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+path(
+        "openapi-schema",
+        get_schema_view(
+            title="Book Show Ticket App",  # Title of your app
+            description="Book show/movie tickets",  # Description of your app
+            version="1.0.0",
+            public=True,
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "docs/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
+    path('auth/', include('auth_app.urls')),
+    path('/api/v1/', include('cinema.urls')),
+    path('api/v1/', include('booking.urls'))
+]
