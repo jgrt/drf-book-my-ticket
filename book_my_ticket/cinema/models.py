95fbf57a6a22bf60d80aca0977d81eb24736553e
jgrt <asati.jagrati93@gmail.com>
2020-09-18 20:31:23 +0530

    Showtime, booking, reservation, ticket models.

--- book_my_ticket/cinema/models.py
+++ book_my_ticket/cinema/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+from auth_app.models import CustomUser
+
 
 class City(models.Model):
     name = models.CharField(max_length=256, blank=False, null=False)
@@ -19,8 +21,6 @@ class Cinema(models.Model):
 
 class Screen(models.Model):
     name = models.CharField(max_length=256, blank=False, null=False)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
 
 
@@ -37,17 +37,37 @@ class SeatCategory(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
 
 
-class Seat(models.Model):
-    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+class ScreenCapacity(models.Model):
     screen = models.ForeignKey(Screen, on_delete=models.CASCADE)
-    category = models.ForeignKey(SeatCategory, on_delete=models.CASCADE)
-    seats = models.IntegerField(blank=False, null=False)
+    seat_category = models.ForeignKey(SeatCategory, on_delete=models.CASCADE)
+    no_of_seats = models.IntegerField(blank=False, null=False, default=50)
+    cost = models.IntegerField(blank=False, null=False, default=200)
 
 
-class MovieInCity(models.Model):
+class Showtime(models.Model):
     movie = models.ForeignKey(Movies, on_delete=models.CASCADE)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    is_available = models.BooleanField(blank=False, null=False, default=True)
+    screen = models.ForeignKey(Screen, on_delete=models.CASCADE)
+    date_time = models.DateTimeField(blank=False, null=False, default=timezone.now)
+
+
+class Booking(models.Model):
+    BOOKING_STATUS = (
+        ('CF', 'CONFIRMED'),
+        ('CC', 'CANCELLED'),
+        ('PF', 'PAYMENT_FAILED')
+    )
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    showtime = models.ForeignKey(Showtime, on_delete=models.CASCADE)
+    seat_category = models.ForeignKey(SeatCategory, on_delete=models.CASCADE)
+    seats = models.IntegerField(blank=False, null=False)
+    status = models.CharField(max_length=2, choices=BOOKING_STATUS)
+
+
+class Ticket(models.Model):
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    amount = models.FloatField()
+
 
 
 
