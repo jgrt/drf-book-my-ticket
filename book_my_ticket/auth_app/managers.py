1e1961ac507604de97e0b09c623b234d0d310ed9
jgrt <asati.jagrati93@gmail.com>
2020-09-22 23:56:08 +0530

    user authentication tests.

--- book_my_ticket/auth_app/managers.py
+++ book_my_ticket/auth_app/managers.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class CustomUserManager(BaseUserManager):
     """
     Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
+    for authentication instead of username.
     """
 
     def create_user(self, email, password, **extra_fields):
