23bb49162bd76a46a66397d858f93a12a38ad691
jgrt <asati.jagrati93@gmail.com>
2020-09-23 04:05:50 +0530

    Added doc-strings to views.

--- book_my_ticket/cinema/views.py
+++ book_my_ticket/cinema/views.py
@@ -2,6 +2,7 @@ import logging
 from django.db.models import Sum
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
@@ -62,148 +63,6 @@ def validate_date_range(payload):
     return start, end
 
 
-class CityDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = City.objects.all()
-    serializer_class = CitySerializer
-
-
-class CityList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = City.objects.all()
-    serializer_class = CitySerializer
-
-
-class BrandDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Brand.objects.all()
-    serializer_class = BrandSerializer
-
-
-class BrandList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Brand.objects.all()
-    serializer_class = BrandSerializer
-
-
-class CinemaDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Cinema.objects.all()
-    serializer_class = CinemaSerializer
-
-
-class CinemaList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Cinema.objects.all()
-    serializer_class = CinemaSerializer
-
-
-class ScreenDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Screen.objects.all()
-    serializer_class = ScreenSerializer
-
-
-class ScreenList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Screen.objects.all()
-    serializer_class = ScreenSerializer
-
-
-class ScreenCapacityList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = ScreenCapacity.objects.all()
-    serializer_class = ScreenCapacitySerializer
-
-
-class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Movies.objects.all()
-    serializer_class = MovieSerializer
-
-
-class MovieList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Movies.objects.all()
-    serializer_class = MovieSerializer
-
-
-class SeatCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = SeatCategory.objects.all()
-    serializer_class = SeatCategorySerializer
-
-
-class SeatCategoryList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = SeatCategory.objects.all()
-    serializer_class = SeatCategorySerializer
-
-
-class ShowtimeList(generics.ListCreateAPIView):
-    permission_classes = [IsAdminUser]
-    queryset = Showtime.objects.all()
-    serializer_class = ShowtimeSerializer
-
-
-class MoviesByCity(generics.GenericAPIView):
-    serializer_class = MoviesByCitySerialazer
-
-    def post(self, request):
-        payload = request.data
-        city = get_object_or_404(City, pk=payload["city"])
-        start, end = validate_date_range(payload)
-        movies_id = Showtime.objects.filter(
-            screen__cinema__city=city, start_timestamp__range=(start, end)
-        ).values_list("movie", flat=True)
-        movies = Movies.objects.filter(id__in=movies_id)
-        return Response(
-            MovieSerializer(list(movies), many=True).data, status=status.HTTP_200_OK
-        )
-
-
-class CinemaByMovie(generics.GenericAPIView):
-    serializer_class = CinemaByMovieSerializer
-
-    def post(self, request):
-        payload = request.data
-        start, end = validate_date_range(payload)
-        movie = get_object_or_404(Movies, pk=payload["movie"])
-        city = get_object_or_404(City, pk=payload["city"])
-        showtimes = Showtime.objects.filter(
-            movie=movie,
-            screen__cinema__city=city,
-            start_timestamp__range=(start, end),
-            status="CF",
-        )
-        screens = Screen.objects.filter(
-            id__in=showtimes.values_list("screen", flat=True)
-        )
-        cinemas = Cinema.objects.filter(id__in=screens.values_list("cinema", flat=True))
-        _cinemas = CinemaWithScreensOnly(list(cinemas), many=True).data
-        s_ids = showtimes.values_list("id", flat=True)
-        resp = list()
-        for cinema in _cinemas:
-            shows = []
-            for co in dict(cinema)["screens"]:
-                _shows = list(
-                    filter(
-                        None,
-                        list(filter(lambda d: d["id"] in s_ids, dict(co)["showtimes"])),
-                    )
-                )
-                if len(_shows) > 0:
-                    shows.extend(_shows)
-
-            _resp = dict(
-                cinema=dict(name=cinema["name"], address=cinema["address"]),
-                showtimes=shows,
-            )
-            resp.append(_resp)
-
-        return Response(resp, status=status.HTTP_200_OK)
-
-
 def check_availability(showtime, seat_category=None):
     if not seat_category:
         seat_categories = (
@@ -245,10 +104,247 @@ def check_availability(showtime, seat_category=None):
     return resp
 
 
+class CityDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get: return a city instance by id(pk).
+    put: update city instance.
+    delete: delete city instance.
+    """
+
+    permission_classes = [IsAdminUser]
+    serializer_class = CitySerializer
+    queryset = City.objects.all()
+
+
+class CityList(generics.ListCreateAPIView):
+    """
+    get: List all the cities.
+    post: Create a new city.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class BrandDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get: return a brand instance by id(pk).
+    put: update brand instance.
+    delete: delete brand instance.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class BrandList(generics.ListCreateAPIView):
+    """
+    get: List all the brands.
+    post: Create a new brand.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class CinemaDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get: return a cinema instance by id(pk).
+    put: update cinema instance.
+    delete: delete cinema instance.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class CinemaList(generics.ListCreateAPIView):
+    """
+    get: List all the cinemas in the system.
+    post: Create a new cinema.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class ScreenDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+   get: return a screen/auditorium instance by id(pk).
+   put: update screen instance.
+   delete: delete screen instance.
+   """
+
+    permission_classes = [IsAdminUser]
+    queryset = Screen.objects.all()
+    serializer_class = ScreenSerializer
+
+
+class ScreenList(generics.ListCreateAPIView):
+    """
+    get: List all the screens.
+    post: Create a new screen.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = Screen.objects.all()
+    serializer_class = ScreenSerializer
+
+
+class ScreenCapacityList(generics.ListCreateAPIView):
+    """
+    get: List all the screen capacity in terms of seats with their category ex-Platinum, Gold, Exclusive etc.
+    post: Add a new screen capacity to screen along seat category.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = ScreenCapacity.objects.all()
+    serializer_class = ScreenCapacitySerializer
+
+
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+   get: return a movie instance by id(pk).
+   put: update movie instance.
+   delete: delete movie instance.
+   """
+
+    permission_classes = [IsAdminUser]
+    queryset = Movies.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieList(generics.ListCreateAPIView):
+    """
+    get: List all the movies in the system.
+    post: Add a new movie.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = Movies.objects.all()
+    serializer_class = MovieSerializer
+
+
+class SeatCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+   get: return a seat category instance by id(pk).
+   put: update seat category instance.
+   delete: delete seat category instance.
+   """
+
+    permission_classes = [IsAdminUser]
+    queryset = SeatCategory.objects.all()
+    serializer_class = SeatCategorySerializer
+
+
+class SeatCategoryList(generics.ListCreateAPIView):
+    """
+    get: List all the seat categories in the system.
+    post: Add a new seat categories.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = SeatCategory.objects.all()
+    serializer_class = SeatCategorySerializer
+
+
+class ShowtimeList(generics.ListCreateAPIView):
+    """
+    get: List all the showtimes in future.
+    post: Add a new showtime.
+    """
+
+    permission_classes = [IsAdminUser]
+    queryset = Showtime.objects.all()
+    serializer_class = ShowtimeSerializer
+
+
+class MoviesByCity(generics.GenericAPIView):
+    serializer_class = MoviesByCitySerialazer
+
+    @swagger_auto_schema(
+        operation_summary="Get all movies currently playing in your chosen city, No Authentication required",
+        operation_description="Accepts the following POST parameters: city_id, start_date(optional, set to current "
+        "date), end_date(optional,set to 7 days later).\nReturn movies list",
+        tags=["end-user"],
+    )
+    def post(self, request):
+        payload = request.data
+        city = get_object_or_404(City, pk=payload["city"])
+        start, end = validate_date_range(payload)
+        movies_id = Showtime.objects.filter(
+            screen__cinema__city=city, start_timestamp__range=(start, end)
+        ).values_list("movie", flat=True)
+        movies = Movies.objects.filter(id__in=movies_id)
+        return Response(
+            MovieSerializer(list(movies), many=True).data, status=status.HTTP_200_OK
+        )
+
+
+class CinemaByMovie(generics.GenericAPIView):
+    serializer_class = CinemaByMovieSerializer
+
+    @swagger_auto_schema(
+        operation_summary="Get all cinemas where movie is playing, No Authentication required",
+        operation_description="Accepts the following POST parameters: "
+        "movie_id, city_id, start_date(optional, set to current date), end_date(optional, "
+        "set to 7 days later).\nReturn cinemas list along with showtime information",
+        tags=["end-user"],
+    )
+    def post(self, request):
+        payload = request.data
+        start, end = validate_date_range(payload)
+        movie = get_object_or_404(Movies, pk=payload["movie"])
+        city = get_object_or_404(City, pk=payload["city"])
+        showtimes = Showtime.objects.filter(
+            movie=movie,
+            screen__cinema__city=city,
+            start_timestamp__range=(start, end),
+            status="CF",
+        )
+        screens = Screen.objects.filter(
+            id__in=showtimes.values_list("screen", flat=True)
+        )
+        cinemas = Cinema.objects.filter(id__in=screens.values_list("cinema", flat=True))
+        _cinemas = CinemaWithScreensOnly(list(cinemas), many=True).data
+        s_ids = showtimes.values_list("id", flat=True)
+        resp = list()
+        for cinema in _cinemas:
+            shows = []
+            for co in dict(cinema)["screens"]:
+                _shows = list(
+                    filter(
+                        None,
+                        list(filter(lambda d: d["id"] in s_ids, dict(co)["showtimes"])),
+                    )
+                )
+                if len(_shows) > 0:
+                    shows.extend(_shows)
+
+            _resp = dict(
+                cinema=dict(name=cinema["name"], address=cinema["address"]),
+                showtimes=shows,
+            )
+            resp.append(_resp)
+
+        return Response(resp, status=status.HTTP_200_OK)
+
+
 class SeatAvailability(generics.RetrieveAPIView):
     lookup_url_kwarg = "showtime_id"
     serializer_class = ReservationSerializer
 
+    @swagger_auto_schema(
+        operation_summary="Check availability of showtime, No Authentication required",
+        operation_description="Accepts the following GET parameters: showtime_id"
+        "\nReturn json response with available seats along with category or message if theatre is full.",
+        tags=["end-user"],
+    )
     def get(self, request, *args, **kwargs):
         showtime_id = self.kwargs.get(self.lookup_url_kwarg)
         showtime = get_object_or_404(Showtime, pk=showtime_id)
@@ -269,6 +365,13 @@ class SeatBooking(generics.CreateAPIView):
     serializer_class = SeatBookingSerializer
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        operation_summary="Book available seat, Authentication is required",
+        operation_description="Accepts the following POST parameters: showtime_id, seat_category, seats(no. of seats)"
+        "\nReturn json response with booked seats along with reservation metadata or message if seats are not "
+        "avaialble at the moment of booking.",
+        tags=["end-user"],
+    )
     def post(self, request, *args, **kwargs):
         payload = request.data
         user = request.user
