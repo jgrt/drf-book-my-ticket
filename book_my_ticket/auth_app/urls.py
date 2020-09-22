78e59b3da236b00bfee745b90936bfe43dd036a5
jgrt <asati.jagrati93@gmail.com>
2020-09-23 04:07:03 +0530

    Changed urls for better tags and sections of APIs.

--- book_my_ticket/auth_app/urls.py
+++ book_my_ticket/auth_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("rest-auth/", include("rest_auth.urls")),
-    path("rest-auth/registration/", include("rest_auth.registration.urls")),
+    path("auth/rest-auth/", include("rest_auth.urls")),
+    path("sign-up/rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
